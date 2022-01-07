f38e4d32908820dbecdd90440ccf31ac3561d9b9
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 01:42:57 -0500

    production file update

--- knexfile.js
+++ knexfile.js
@@ -23,18 +23,13 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
-    connection: {
-      database: process.env.NODE_ENV,
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+        directory: __dirname + "/database/migrations",
+    },
+    seeds: {
+        directory: __dirname + "/database/seeds/production",
+    },
+}
 };
