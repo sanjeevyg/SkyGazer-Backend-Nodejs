3ea787c677bb12c62b70a74d32fcdfe6545814aa
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 01:37:04 -0500

    production changed

--- knexfile.js
+++ knexfile.js
@@ -6,4 +6,35 @@ module.exports = {
     client: 'pg',
     connection: "postgress:///skyGazer"
   },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: process.env.NODE_ENV,
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
