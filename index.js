fbd8a9fa1810226a46acb6ac63b84c0748388273
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 01:29:48 -0500

    database added

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const bodyParser = require("body-parser")
 const userRouter = require('./routes/user.js')
 
 const app = express();
-const port = process.env.PORT
-app.listen(port, () => console.log(`listening at port ${port}`))
 
 
 app.use(cors())
 app.use(bodyParser.json())
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`listening at port ${port}`))
 app.use(userRouter)
 
 
--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: 'skyGazer',
       user:     'username',
       password: 'password'
     },
