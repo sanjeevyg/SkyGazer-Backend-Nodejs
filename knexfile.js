a1d063767f279655031da243466aa759b668d568
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 01:32:09 -0500

    knexfile updated

--- knexfile.js
+++ knexfile.js
@@ -6,35 +6,4 @@ module.exports = {
     client: 'pg',
     connection: "postgress:///skyGazer"
   },
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'skyGazer',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 };
