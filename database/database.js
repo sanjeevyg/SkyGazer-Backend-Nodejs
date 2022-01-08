362777e5ae9a458b2164ee6ec8beac980f01ea64
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 22:21:44 -0500

    test 05

--- database/database.js
+++ database/database.js
@@ -2,4 +2,24 @@ const knex = require('knex')
 const config = require('../knexfile')[process.env.NODE_ENV || "development"]
 
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+
 module.exports = knex(config);
