091a94124ea7f2186de5258b880dd436da60d153
sanjeevyg <yogi.sjv@gmail.com>
2022-01-07 22:18:31 -0500

    test 04

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ const { response } = require('express')
 
 const app = express();
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 app.use(cors())
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: false }))
