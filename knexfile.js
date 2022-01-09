85387a1a6d82a21e862c66b7088ea06044791681
sanjeevyg <yogi.sjv@gmail.com>
2022-01-08 23:50:05 -0500

    test 32

--- knexfile.js
+++ knexfile.js
@@ -8,17 +8,15 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
-    },
-    pool: {
-      min: 2, 
-      max: 10
     },
     migrations: {
       tablename: 'knex_migrations',
-      directory: './migrations',
+      directory: './migrations', 
     },
     seeds: { directory: './seeds' }
   }
+}
   
