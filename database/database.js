d09954b0851ca4e09a7e4f17295478faa3ed872d
sanjeevyg <yogi.sjv@gmail.com>
2022-01-08 19:45:53 -0500

    test 10

--- database/database.js
+++ database/database.js
@@ -1,5 +1,6 @@
 const knex = require('knex')
 const config = require('../knexfile')[process.env.DB_ENV || "development"]
+const config = require('../knexfile')[production || "development"]
 
 
 module.exports = knex(config);
