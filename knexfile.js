80ec546f26bfcb10a634bb93d84c89c3d83f9316
sanjeevyg <yogi.sjv@gmail.com>
2022-01-08 23:40:56 -0500

    test 30

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      ssl: { rejectUnauthorized: true },
     },
     pool: {
       min: 2, 
